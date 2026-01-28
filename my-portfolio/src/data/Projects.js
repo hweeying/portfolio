3a57b16f21469a2f1be1246bb7ab833bb5f21a41
Jasmine <124573638+hweeying@users.noreply.github.com>
2026-01-29 00:21:27 +0800

    feat(work): Add new project

--- my-portfolio/src/data/Projects.js
+++ my-portfolio/src/data/Projects.js
@@ -3,14 +3,16 @@ export const projects = [
     id: '01',
     title: 'Whisel: Sports Booking Mobile App',
     description: 'Designed and developed a Flutter-based football field booking app, implementing UI/UX, REST API integration, local state management, and Firebase Cloud Messaging for push notifications.',
-    tags: [
-      'Flutter',
-      'iOS, Android',
-      'RESTful API',
-      'Firebase Cloud Messaging',
-      'Firebase Crashlytics',
-    ],
+    tags: ['Flutter', 'RESTful API', 'Firebase'],
     href: 'https://play.google.com/store/apps/details?id=my.com.fivek.whisel',
     image: '/work/whisel.png', // ← NEW
+  },
+  {
+    id: '02',
+    title: 'Netflix Clone',
+    description: 'Netflix clone built with React, Tailwind CSS, and TMDB API, featuring dynamic content, horizontal carousels.',
+    tags: ['React', 'TailwindCSS', 'RESTful API'],
+    href: 'https://jlhy-netflix-clone.vercel.app/',
+    image: '/work/netflix-clone.png', // ← NEW
   },
 ];
